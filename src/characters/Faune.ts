29291e374d72cdf072c1032b394d5bc6cbe084e4
webdevmike42 <GRAFWERN@brzg.local>
2024-08-29 15:30:46 +0200

    added damage animation to character and did refactoring for character code

---
+++ src/characters/Faune.ts
@@ -0,0 +1,109 @@
+import Phaser from "phaser";
+declare global {
+    namespace Phaser.GameObjects {
+        interface GameObjectFactory {
+            faune(x: number, y: number, texture: string, frame?: string | number): Faune
+        }
+    }
+}
+
+enum HealthStates {
+    IDLE,
+    DAMAGE
+}
+
+export default class Faune extends Phaser.Physics.Arcade.Sprite {
+    private healthState = HealthStates.IDLE;
+    private damageTime = 0;
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
+        super(scene, x, y, texture, frame);
+
+        this.anims.play("faune-idle-down");
+    }
+
+    handleDamage(dir: Phaser.Math.Vector2) {
+        if (this.healthState === HealthStates.DAMAGE)
+            return;
+
+        this.setVelocity(dir.x, dir.y);
+
+        this.setTint(0xff0000);
+        this.healthState = HealthStates.DAMAGE;
+        this.damageTime = 0;
+    }
+
+    preUpdate(t: number, dt: number) {
+        super.preUpdate(t,dt);
+
+        switch (this.healthState) {
+            case HealthStates.IDLE:
+                break;
+            case HealthStates.DAMAGE:
+                this.damageTime += dt;
+                if (this.damageTime >= 250) {
+                    this.healthState = HealthStates.IDLE;
+                    this.clearTint();
+                    this.damageTime = 0;
+                }
+                break;
+        }
+
+    }
+
+    update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+        
+        if (!cursors || this.healthState === HealthStates.DAMAGE)
+            return;
+
+        const speed: number = 100;
+        let vx = 0;
+        let vy = 0;
+
+        if (cursors.left?.isDown) {
+            vx = -speed;
+            this.scaleX = -1;
+            if (this.body !== null) this.body.offset.x = 24;
+        }
+        if (cursors.right?.isDown) {
+            vx = speed;
+            this.scaleX = 1;
+            if (this.body !== null) this.body.offset.x = 8;
+        }
+        if (cursors.up?.isDown) {
+            vy = -speed;
+        }
+        if (cursors.down?.isDown) {
+            vy = speed;
+        }
+
+        if (vx !== 0 || vy !== 0) {
+            this.setVelocity(vx, vy);
+            if (vx !== 0) {
+                this.anims.play("faune-walk-side", true);
+            } else if (vy < 0) {
+                this.anims.play('faune-walk-up', true);
+            } else if (vy > 0) {
+                this.anims.play('faune-walk-down', true);
+            }
+        } else {
+            this.setVelocity(0, 0);
+            const parts: string[] = this.anims.currentAnim?.key.split('-') || [];
+            parts[1] = 'idle';
+            this.anims.play(parts.join('-'));
+        }
+    }
+}
+
+Phaser.GameObjects.GameObjectFactory.register('faune', function (this: Phaser.GameObjects.GameObjectFactory, x: number, y: number, texture: string, frame?: string | number) {
+    let sprite = new Faune(this.scene, x, y, texture, frame);
+
+    this.displayList.add(sprite);
+    this.updateList.add(sprite);
+
+    this.scene.physics.world.enableBody(sprite, Phaser.Physics.Arcade.DYNAMIC_BODY);
+
+    sprite.body?.setSize(sprite.width * 0.5, sprite.height * 0.8);
+
+    return sprite;
+})
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,15 +2,16 @@ import { Scene } from 'phaser';
 import { debugDraw } from '../utils/debug';
 import { createLizardAnims } from '../anims/EnemyAnims';
 import { createCharacterAnims } from '../anims/CharacterAnims';
-import Lizard from './enemies/lizard';
-
+import Lizard from './enemies/Lizard';
+import '../characters/Faune';
+import Faune from '../characters/Faune';
 
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     msg_text: Phaser.GameObjects.Text;
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-    private faune!: Phaser.Physics.Arcade.Sprite;
+    private faune!: Faune;
 
     constructor() {
         super('Game');
@@ -43,26 +44,27 @@ export class Game extends Scene {
 
                 debugDraw(wallsLayer, this);
 
-                this.faune = this.physics.add.sprite(128, 128, "faune", "walk-side-3.png");
-                this.faune.setSize(16, 32);
+                
+                this.faune = this.add.faune(128,128,"faune", "walk-side-3.png")
+                
                 this.physics.add.collider(this.faune, wallsLayer);
 
                 const lizards = this.physics.add.group({
                     classType: Lizard,
                     createCallback: (go) => {
                         const lizGo = go as Lizard;
-                        if(lizGo.body)
+                        if (lizGo.body)
                             lizGo.body.onCollide = true;
                     }
                 })
 
-                lizards.get(256,128, "lizard");
+                lizards.get(256, 128, "lizard");
                 
                 this.physics.add.collider(lizards, wallsLayer);
+
+                this.physics.add.collider(lizards, this.faune, this.handlePlayerLizardCollision, undefined, this);
             }
         }
-
-        this.faune.anims.play("faune-idle-down");
 
         this.camera.startFollow(this.faune, true);
 
@@ -73,45 +75,19 @@ export class Game extends Scene {
         });
     }
 
+    private handlePlayerLizardCollision(obj1: any, obj2: any) {
+        const lizard = obj2 as Lizard;
+
+        const dx = this.faune.x - lizard.x;
+        const dy = this.faune.y - lizard.y
+
+        const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200);
+
+        this.faune.setVelocity(dir.x, dir.y);
+        this.faune.handleDamage(dir);
+    }
+
     update(t: number, dt: number) {
-        if (!(this.cursors && this.faune))
-            return;
-
-        const speed: number = 100;
-        let vx = 0;
-        let vy = 0;
-
-        if (this.cursors.left?.isDown) {
-            vx = -speed;
-            this.faune.scaleX = -1;
-            if (this.faune.body !== null) this.faune.body.offset.x = 24;
-        }
-        if (this.cursors.right?.isDown) {
-            vx = speed;
-            this.faune.scaleX = 1;
-            if (this.faune.body !== null) this.faune.body.offset.x = 8;
-        }
-        if (this.cursors.up?.isDown) {
-            vy = -speed;
-        }
-        if (this.cursors.down?.isDown) {
-            vy = speed;
-        }
-
-        if (vx !== 0 || vy !== 0) {
-            this.faune.setVelocity(vx, vy);
-            if (vx !== 0) {
-                this.faune.anims.play('faune-walk-side', true);
-            } else if (vy < 0) {
-                this.faune.anims.play('faune-walk-up', true);
-            } else if (vy > 0) {
-                this.faune.anims.play('faune-walk-down', true);
-            }
-        } else {
-            this.faune.setVelocity(0, 0);
-            const parts: string[] = this.faune.anims.currentAnim?.key.split('-') || [];
-            parts[1] = 'idle';
-            this.faune.anims.play(parts.join('-'));
-        }
+        this.faune.update(this.cursors);
     }
 }
